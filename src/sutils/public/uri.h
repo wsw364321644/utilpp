36e02ae2db351778016ec48b7d5e959ab74ac753
sheng_wei.wan <sheng_wei.wan@ceasia.cn>
2023-04-12 15:01:05 +0800

    fix regex

--- src/sutils/public/uri.h
+++ src/sutils/public/uri.h
@@ -1,10 +1,19 @@
 #pragma once
 #include <string>
 #include <regex>
-inline void ParseUrl(const std::string& inurl, std::string* outScheme = nullptr, std::string* outAuthority = nullptr, std::string* outPort = nullptr, std::string* outPath = nullptr, std::string* outQuery = nullptr, std::string* outFragment = nullptr) {
+typedef struct ParsedURL_t
+{
+    std::string* outScheme{ nullptr };
+    std::string* outAuthority{ nullptr };
+    std::string* outPort{ nullptr };
+    std::string* outPath{ nullptr };
+    std::string* outQuery { nullptr };
+    std::string* outFragment  { nullptr };
+}ParsedURL_t;
+inline void ParseUrl(const std::string& inurl, ParsedURL_t* pURL) {
     //https://www.rfc-editor.org/rfc/rfc3986#page-50
     std::regex url_regex(
-        R"(^(([^:\/?#]+):\/\/)?(([^:\/?#]*)(:([0-9]+))?)?([^:?#]*)(\?([^#]*))?(#(.*))?)",
+        R"(^(([^:\/?#]+)://)?(([^:\/?#]*)(:([0-9]+))?)?([^:?#]*)(\?([^#]*))?(#(.*))?)",
         std::regex::extended
     );
     std::smatch url_match_result;
@@ -13,22 +22,22 @@ inline void ParseUrl(const std::string& inurl, std::string* outScheme = nullptr,
     if (!std::regex_match(inurl, url_match_result, url_regex)) {
         return;
     }
-    if (outScheme) {
-        *outScheme = url_match_result[2];
+    if (pURL->outScheme) {
+        *pURL->outScheme = url_match_result[2];
     }
-    if (outAuthority) {
-        *outAuthority = url_match_result[4];
+    if (pURL->outAuthority) {
+        *pURL->outAuthority = url_match_result[4];
     }
-    if (outPort) {
-        *outPort = url_match_result[6];
+    if (pURL->outPort) {
+        *pURL->outPort = url_match_result[6];
     }
-    if (outPath) {
-        *outPath = url_match_result[7];
+    if (pURL->outPath) {
+        *pURL->outPath = url_match_result[7];
     }
-    if (outQuery) {
-        *outQuery = url_match_result[9];
+    if (pURL->outQuery) {
+        *pURL->outQuery = url_match_result[9];
     }
-    if (outFragment) {
-        *outFragment = url_match_result[11];
+    if (pURL->outFragment) {
+        *pURL->outFragment = url_match_result[11];
     }
 }
