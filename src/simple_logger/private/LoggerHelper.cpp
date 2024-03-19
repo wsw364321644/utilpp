447574bfd98b45655aa98f3c8d49aa5db97fd995
Savion Wan <364321644@qq.com>
2024-03-19 19:47:15 +0800

    add log helper

---
+++ src/simple_logger/private/LoggerHelper.cpp
@@ -0,0 +1,130 @@
+#include "LoggerHelper.h"
+
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/msvc_sink.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+#include <spdlog/async.h>
+#include <module_util.h>
+#include <mutex>
+#include <variant>
+#include <filesystem>
+#include <format>
+static std::mutex CreateLoggerMtx;
+
+
+
+template <typename T, 
+    typename std::enable_if<std::is_same<typename std::decay<T>::type, std::filesystem::path>::value>::type* = nullptr>
+spdlog::sink_ptr GetSamePathSink(T&& path) {
+    spdlog::sink_ptr psink;
+    spdlog::details::registry::instance().apply_all(
+        [&path, &psink](const std::shared_ptr<spdlog::logger> itr) {
+            auto sinks = itr->sinks();
+            for (auto& sinkitr : sinks) {
+                auto pRotateSink = std::dynamic_pointer_cast<spdlog::sinks::rotating_file_sink_mt>(sinkitr);
+                if (pRotateSink) {
+                    if (std::filesystem::path(pRotateSink->filename()) == path) {
+                        psink = sinkitr;
+                        return;
+                    }
+                }
+            }
+        }
+    );
+    return psink;
+}
+
+std::shared_ptr<FLoggerWrapper> InternalCreateAsyncLogger(const LoggerSetting_t& setting)
+{
+    std::unique_lock lock(CreateLoggerMtx);
+    auto logger = spdlog::get(setting.Name);
+    if (logger)
+    {
+        return std::make_shared<FLoggerWrapper>(logger);
+    }
+    if (!spdlog::thread_pool()) {
+        spdlog::init_thread_pool(8192, 1);
+    }
+    std::vector<spdlog::sink_ptr> sinks;
+    for (auto& pinfo : setting.Infos) {
+        auto pStdoutLoggerInfo= std::dynamic_pointer_cast<StdoutLoggerInfo_t>(pinfo);
+        if (pStdoutLoggerInfo) {
+            sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+            continue;
+        }
+        auto pMSVCLoggerInfo = std::dynamic_pointer_cast<MSVCLoggerInfo_t>(pinfo);
+        if (pMSVCLoggerInfo) {
+            sinks.push_back(std::make_shared<spdlog::sinks::msvc_sink_mt>());
+            continue;
+        }
+        auto pRotatingLoggerInfo = std::dynamic_pointer_cast<RotatingLoggerInfo_t>(pinfo);
+        if (pRotatingLoggerInfo) {
+            std::filesystem::path p(pRotatingLoggerInfo->FilePath);
+            if (!p.has_filename()) {
+                continue;
+            }
+            auto pSamePathSink=GetSamePathSink(std::filesystem::path(pRotatingLoggerInfo->FilePath));
+            if (pSamePathSink) {
+                sinks.push_back(pSamePathSink);
+            }
+            else {
+                sinks.push_back(std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+                    pRotatingLoggerInfo->FilePath, pRotatingLoggerInfo->FileSize, pRotatingLoggerInfo->FileNum, pRotatingLoggerInfo->RotateOnOpen));
+            }
+            continue;
+        }
+    }
+
+    logger = std::make_shared<spdlog::async_logger>(setting.Name, sinks.begin(), sinks.end(),
+        spdlog::thread_pool(), spdlog::async_overflow_policy::block);
+
+    if (!setting.Name.empty()) {
+        logger->set_pattern("[%D %T %z] [%n] [%^---%L---%$] [%s:%!] %v");
+    }
+    spdlog::register_logger(logger);
+
+    return std::make_shared<FLoggerWrapper>(logger);
+}
+
+std::shared_ptr<FLoggerWrapper> CreateDefaultFileLogger(std::string name) {
+    size_t size{ 0 };
+    util_exe_wpath(nullptr, &size);
+    auto pathstr = new wchar_t[++size];
+    util_exe_wpath(pathstr, &size);
+    std::filesystem::path path(pathstr);
+    delete pathstr;
+    auto stru8=path.stem().u8string();
+    stru8.append(u8".log");
+
+    std::string str(stru8.cbegin(), stru8.cend());
+    auto pWrapper = InternalCreateAsyncLogger({ name, {std::make_shared<RotatingLoggerInfo_t>(str),std::make_shared<MSVCLoggerInfo_t>(),std::make_shared<StdoutLoggerInfo_t>()} });
+    if (name.empty()) {
+        spdlog::set_default_logger(pWrapper->Logger);
+    }
+#ifdef NDEBUG
+    pWrapper->set_level(spdlog::level::warn);
+#else
+    pWrapper->set_level(spdlog::level::trace);
+#endif // DEBUG
+
+    return pWrapper;
+}
+std::shared_ptr<FLoggerWrapper> CreateAsyncLogger(const LoggerSetting_t& setting)
+{
+    auto logger = spdlog::get(setting.Name);
+    if (logger){
+        return std::make_shared<FLoggerWrapper>(logger);
+    }
+    return InternalCreateAsyncLogger(setting);
+}
+
+std::shared_ptr<FLoggerWrapper> GetLogger(std::string name)
+{
+    auto logger = spdlog::get(name);
+    if (logger)
+    {
+        return std::make_shared<FLoggerWrapper>(logger);
+    }
+    return CreateDefaultFileLogger(name);
+}
+
---
+++ src/simple_logger/public/LoggerHelper.h
@@ -0,0 +1,91 @@
+#pragma once
+#include <string>
+#include <memory>
+#include <vector>
+
+#include <spdlog/spdlog.h>
+struct BaseLoggerInfo_t{
+    virtual ~BaseLoggerInfo_t() {}
+};
+struct StdoutLoggerInfo_t: BaseLoggerInfo_t {
+};
+struct MSVCLoggerInfo_t : BaseLoggerInfo_t {
+};
+
+struct FileLoggerInfo_t : BaseLoggerInfo_t {
+    FileLoggerInfo_t() {}
+    FileLoggerInfo_t(std::string str) :FilePath(str) {}
+    std::string FilePath;
+};
+struct RotatingLoggerInfo_t:FileLoggerInfo_t {
+    RotatingLoggerInfo_t() {}
+    RotatingLoggerInfo_t(std::string str) :FileLoggerInfo_t(str ) {}
+    int FileSize{ 1024 * 1024 * 5 };
+    int FileNum{1};
+    bool RotateOnOpen{ false };
+};
+
+struct LoggerSetting_t{
+    std::string Name;
+    std::vector<std::shared_ptr<BaseLoggerInfo_t>> Infos;
+};
+
+
+class FLoggerWrapper {
+public:
+    FLoggerWrapper(std::shared_ptr<spdlog::logger> logger) :Logger(logger) {
+    }
+    ~FLoggerWrapper() {
+    }
+
+    template <typename... Args>
+    void trace(std::format_string<Args...> fmt, Args &&...args) {
+        Logger->trace(fmt, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    void debug(std::format_string<Args...> fmt, Args &&...args) {
+        Logger->debug(fmt, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    void info(std::format_string<Args...> fmt, Args &&...args) {
+        Logger->log(fmt, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    void warn(std::format_string<Args...> fmt, Args &&...args) {
+        Logger->warn(fmt, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    void error(std::format_string<Args...> fmt, Args &&...args) {
+        Logger->error(fmt, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    void critical(std::format_string<Args...> fmt, Args &&...args) {
+        Logger->critical(fmt, std::forward<Args>(args)...);
+    }
+
+    void set_level(spdlog::level::level_enum log_level) {
+        Logger->set_level(log_level);
+    }
+
+    template <typename... Args>
+    void log(spdlog::source_loc loc, spdlog::level::level_enum lvl, std::format_string<Args...> fmt, Args &&...args) {
+        Logger->log(loc,lvl,fmt, std::forward<Args>(args)...);
+    }
+
+    std::shared_ptr<spdlog::logger> Logger;
+};
+std::shared_ptr<FLoggerWrapper> CreateAsyncLogger(const LoggerSetting_t& setting);
+std::shared_ptr<FLoggerWrapper> GetLogger(std::string name = "");
+
+#define SIMPLELOG_LOGGER_TRACE(name ,...) GetLogger(name)->log(spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::level_enum::trace, __VA_ARGS__)
+#define SIMPLELOG_LOGGER_DEGUB(name ,...) GetLogger(name)->log(spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::level_enum::debug, __VA_ARGS__)
+#define SIMPLELOG_LOGGER_INFO(name ,...) GetLogger(name)->log(spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::level_enum::info, __VA_ARGS__)
+#define SIMPLELOG_LOGGER_WARN(name ,...) GetLogger(name)->log(spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::level_enum::warn, __VA_ARGS__)
+#define SIMPLELOG_LOGGER_ERROR(name ,...) GetLogger(name)->log(spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::level_enum::err, __VA_ARGS__)
+#define SIMPLELOG_LOGGER_CRIRICAL(name ,...) GetLogger(name)->log(spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::level_enum::critical, __VA_ARGS__)
+
