a6c91af488c873676c4bbc32e122818869c8528f
Savion Wan <364321644@qq.com>
2024-03-29 15:52:29 +0800

    add os_gettime_ns

---
+++ src/simple_util/private/windows/simple_time_win.c
@@ -0,0 +1,15 @@
+#include "simple_time.h"
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+uint64_t os_gettime_ns(void)
+{
+	LARGE_INTEGER current_time;
+	double time_val;
+
+	QueryPerformanceCounter(&current_time);
+	time_val = (double)current_time.QuadPart;
+	time_val *= 1000000000.0;
+	time_val /= (double)get_clockfreq();
+
+	return (uint64_t)time_val;
+}
---
+++ src/simple_util/public/simple_time.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "simple_export_defs.h"
+#include <stdint.h>
+SIMPLE_UTIL_API uint64_t os_gettime_ns(void);
