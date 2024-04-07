c2b02905fb44272334de1051d66668bf93543c66
Savion Wan <364321644@qq.com>
2024-04-07 11:06:51 +0800

    fix  WriteSharedMemory declare

--- src/ipc_util/public/sm_util.h
+++ src/ipc_util/public/sm_util.h
@@ -6,6 +6,6 @@ CommonHandle_t* OpenSharedMemory(const char* name);
 void* MapSharedMemory(CommonHandle_t* phandle);
 void* MapReadSharedMemory(CommonHandle_t* phandle);
 void UnmapSharedMemory(void* ptr);
-bool WriteSharedMemory(CommonHandle_t* phandle, void* content, size_t*);
+bool WriteSharedMemory(CommonHandle_t* phandle, void* content, size_t);
 bool ReadSharedMemory(CommonHandle_t* phandle, void* content, size_t*);
 void CloseSharedMemory(CommonHandle_t* phandle);
