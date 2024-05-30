9c4199c28e2e4d96288e2b3e485240afdc2dbf94
Savion Wan <364321644@qq.com>
2024-05-30 11:28:09 +0800

    add string_hash

--- src/simple_utilpp/public/std_ext.h
+++ src/simple_utilpp/public/std_ext.h
@@ -4,6 +4,7 @@
 #include <variant>
 #include <stdexcept>
 
+
 template <class T>
 inline void hash_combine(std::size_t &seed, const T &v)
 {
@@ -61,3 +62,14 @@ namespace std {
 }
 #endif
 #endif // _MSVC_LANG
+
+
+struct string_hash
+{
+    using hash_type = std::hash<std::string_view>;
+    using is_transparent = void;
+
+    std::size_t operator()(const char* str) const { return hash_type{}(str); }
+    std::size_t operator()(std::string_view str) const { return hash_type{}(str); }
+    std::size_t operator()(std::string const& str) const { return hash_type{}(str); }
+};
