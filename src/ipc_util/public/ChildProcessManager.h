c0111696a62feb3d79b5a4f5da19d52fc3add09e
Savion Wan <364321644@qq.com>
2025-02-08 16:16:36 +0800

    use int64_t instead ssize_t libuv defined ssize_t in its win.h

--- src/ipc_util/public/ChildProcessManager.h
+++ src/ipc_util/public/ChildProcessManager.h
@@ -4,17 +4,19 @@
 #include <unordered_map>
 #include <memory>
 #include <functional>
-#if !defined(_SSIZE_T_) && !defined(_SSIZE_T_DEFINED)
-#ifdef _MSC_VER
-#include <BaseTsd.h>
-typedef SSIZE_T ssize_t;
-# define SSIZE_MAX INTPTR_MAX
-# define _SSIZE_T_
-# define _SSIZE_T_DEFINED
-#else
-#include <unistd.h>
-#endif
-#endif
+
+//#ifdef _MSC_VER
+//#if !defined(_SSIZE_T_) && !defined(_SSIZE_T_DEFINED)
+//#include <BaseTsd.h>
+//typedef SSIZE_T ssize_t;
+//# define SSIZE_MAX INTPTR_MAX
+//# define _SSIZE_T_
+//# define _SSIZE_T_DEFINED
+//#endif
+//#else
+//#include <unistd.h>
+//#endif
+
 
 #include "ipc_util_export.h"
 typedef struct uv_loop_s uv_loop_t;
@@ -31,7 +33,7 @@ public:
     ~FChildProcessManager() {}
     static FChildProcessManager* GetInstance();
     CommonHandle_t SpawnProcess(const char* filepath,const char** args=nullptr);
-    typedef std::function< void (CommonHandle_t,const char* , ssize_t) > FOnReadDelegate;
+    typedef std::function< void (CommonHandle_t,const char* , int64_t) > FOnReadDelegate;
     void RegisterOnRead(CommonHandle_t handle, FOnReadDelegate delegate);
     typedef std::function< void(CommonHandle_t,int64_t, int) > FOnExitDelegate;
     void RegisterOnExit(CommonHandle_t handle, FOnExitDelegate delegate);
