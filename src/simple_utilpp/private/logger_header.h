637f9db58950acf0374644bbeacfab982bd48ef5
Savion Wan <364321644@qq.com>
2025-12-02 11:18:42 +0800

    fix warning

--- src/simple_utilpp/private/logger_header.h
+++ src/simple_utilpp/private/logger_header.h
@@ -1,7 +1,8 @@
 #ifdef UTILPP_ENABLE_LOG
 #include <LoggerHelper.h>
 #else
-#define SIMPLELOG_LOGGER_INFO(name ,...)(__VA_ARGS__)
-#define SIMPLELOG_LOGGER_WARN(name ,...) (__VA_ARGS__)
-#define SIMPLELOG_LOGGER_ERROR(name ,...) (__VA_ARGS__)
+//#define SIMPLELOG_LOGGER_INFO(name ,...) (__VA_ARGS__)
+#define SIMPLELOG_LOGGER_INFO(name ,...)
+#define SIMPLELOG_LOGGER_WARN(name ,...)
+#define SIMPLELOG_LOGGER_ERROR(name ,...)
 #endif // UTILPP_ENABLE_LOG
