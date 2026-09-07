1f25b230f82dfcb2c64e590e0e18d8eedfeeb52d
Savion Wan <364321644@qq.com>
2026-09-07 20:05:23 +0800

    add htobe32 htobe16

--- src/simple_util/private/windows/endian_helper_win.c
+++ src/simple_util/private/windows/endian_helper_win.c
@@ -2,6 +2,17 @@
 #include "simple_os_defs.h"
 
 #include <winsock2.h>
+#pragma comment(lib, "Ws2_32.lib")
 uint64_t htobe64(uint64_t host_64bits) {
     return htonll(host_64bits);
+}
+
+uint64_t htobe32(uint32_t host_bits)
+{
+    return htonl(host_bits);
+}
+
+uint64_t htobe16(uint16_t host_bits)
+{
+    return htons(host_bits);
 }
--- src/simple_util/public/endian_helper.h
+++ src/simple_util/public/endian_helper.h
@@ -3,6 +3,8 @@
 #include <stdint.h>
 #ifdef WIN32
 SIMPLE_UTIL_API uint64_t htobe64(uint64_t host_64bits);
+SIMPLE_UTIL_API uint64_t htobe32(uint32_t host_bits);
+SIMPLE_UTIL_API uint64_t htobe16(uint16_t host_bits);
 
 #else
 #include <endian.h>
